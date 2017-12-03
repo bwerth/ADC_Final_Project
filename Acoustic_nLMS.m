2332ae01763202fd63096773f6ca76c179a47899
JosephLee19 <leejos17@gmail.com>
2017-12-03 11:13:39 -0500

    normalized LMS works

--- Acoustic_nLMS.m
+++ Acoustic_nLMS.m
@@ -1,34 +1,30 @@
 clear
+close all
 
-load('handel')
-%y = handel music signal
-%Fs = sample frequency
-step_size=0.001;%Constant for normalized LMS
-a=1;%bias for normalized LMS algorithm
-noise_signal=.75; %Desired rms amplitude of noise relative to rms amplitude of signal
-noise=(noise_signal*rms(y))*randn(size(y)); %Randomly generated gaussian white noise with rms amplitude of noise_signal
-H_s=[.1 .7 .1];% impulse response of system (we just made this up for testing purposes)
-%H_s=[1];
-noise_ear=conv(noise,H_s,'same');% noise passed through this channel
-noisy_audio=y+noise_ear;
-LMS_order=500;% Order of the LMS - i.e. the size of the weight vector
-W_s=zeros(1,length(y));% initialize weight vector of 0s with size LMS order
-error=zeros(1,length(y));
-x=y+noise;
-for i=LMS_order:length(y)
-    anti_noise(i) = W_s(i-LMS_order+1:i)*noise(i-LMS_order+1:i);
-    error(i) = noise_ear(i)-anti_noise(i);
-    W_s(i+1) = W_s(i) + step_size*error(i)*x(i)/((x(i))^2+a);
+N=10000;    %Length of input in samples
+t=[0:N-1];  %vector N integers incrementing from 0
+w0=0.001;   %frequency of sinewave test input
+phi=0.1;    %phase for sinewave test input
+d=sin(2*pi*[1:N]*w0+phi);   %desired signal in our case just a sinewave
+x=d+randn(1,N)*0.5;         %signal with noise                 %step size
+a = 1;                      %bias for normalized LMS
+M = 50;                     %LMS filter order
+w=zeros(M,N);               %initialize weight vector
+u=.1;                    %adaptation rate constant
+for i=(M+1):N
+   e(i) = d(i) -  x((i-(M)+1):i)*w(:,i);    %error calculation
+   mu = u/(x((i-(M)+1):i)*x((i-(M)+1):i)' + a);     %step size normalization
+   w(:,i+1) = w(:,i) + mu * e(i) * x((i-(M)+1):i)'; %weight update equation
 end
-W_s=W_s(2:end);
-anti_noise=anti_noise';
-plot(anti_noise+noisy_audio)
-for i=1:73113
-    predicted(i)=W_s(i)*noise(i);
+for i=(M+1):N
+    yd(i) = x((i-(M)+1):i)*w(:,i);  %calculate the filtered noisy input
 end
-subplot(311);
-plot(predicted)
-subplot(312);
-plot(noise_ear)
-subplot(313);
-plot(noise_ear'-predicted)
+
+for i=1:length(e)
+    mse(i)= mean(e(1:i).^2);    %calculate mean squared error for plotting
+end
+
+subplot(221),plot(t,d),ylabel('Desired Signal'),
+subplot(222),plot(t,x),ylabel('Input Signal+Noise'),
+subplot(223),plot(t,mse),ylabel('Mean Squared Error'),
+subplot(224),plot(t,yd),ylabel('Adaptive Desired output');
